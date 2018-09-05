a0894f8b0baee5d6275111e0d63df3178af1c038
yugantar jain <yugantarjain@yujs-macbook-pro.local>
2018-09-05 17:46:42 +0530

    First Commit

--- BVPIEEE/AppDelegate.swift
+++ BVPIEEE/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuthUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let authUI = FUIAuth.defaultAuthUI()
         return true
     }
 
--- BVPIEEE/SignInViewController.swift
+++ BVPIEEE/SignInViewController.swift
@@ -9,20 +9,42 @@
 import UIKit
 import Firebase
 import FirebaseAuthUI
+import FirebaseGoogleAuthUI
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         FirebaseApp.configure()
-        let authUI = FUIAuth.defaultAuthUI()        
+        let authUI = FUIAuth.defaultAuthUI()
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth()
+        ]
+        authUI?.providers = providers
+        let authViewController = authUI?.authViewController()
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool
+    {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false
+        {
+            return true
+        }
+        // other URL handling goes here.
+        return false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        // handle user and error as necessary
     }
     
 
