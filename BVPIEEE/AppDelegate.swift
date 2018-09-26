f675ddf69d8446a74065b16ec2125f768aafe1da
yugantar jain <yugantarjain@yujs-macbook-pro.local>
2018-09-27 00:03:08 +0530

    Sign in removed, errors removed.

--- BVPIEEE/AppDelegate.swift
+++ BVPIEEE/AppDelegate.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Firebase
-import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+        //FirebaseApp.configure()
         return true
     }
     
--- BVPIEEE/SecondViewController.swift
+++ BVPIEEE/SecondViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController  {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
